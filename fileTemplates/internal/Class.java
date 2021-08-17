8cece3b3cd305248a751cfc91befffb44d547cdc
zengsl <zengsl0212@foxmail.com>
2021-08-17 14:02:36 +0800

    IU-2021.2 <zengsl@zengsl-MacBook-Pro.local Update baseRefactoring.xml, GrepConsole.xml, CodeGlance.xml, project.default.xml, diff.xml, macOS copy.xml, Default.xml, ignore.xml, find.xml, ui.lnf.xml, filetypes.xml, vcs.xml, colors.scheme.xml, KeyPromoterXSettings.xml, databaseDrivers.xml, editor.xml, usageView.xml, debugger.xml	Create laf.xml, ide.general.xml, mybatisx.xml, jprofiler.xml, ide-features-trainer.xml, Enum.java, Interface.java, lombok-plugin.xml, yiiguxing.translation.xml, docker.xml, dracula-theme.xml, Class.java, jdk.table.xml, visualizationTool.xml, zzz.xml, github.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+ * ${Description} 
+ * @author zengsl
+ * @date $DATE $TIME
+ * @version V1.0
+ */
+public class ${NAME} {
+}
